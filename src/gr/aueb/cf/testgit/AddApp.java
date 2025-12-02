969210d6aff693e763962cdafe6e4f41ec926b95
vaggpappas <vag_pappas@hotmail.com>
2025-12-02 19:07:26 +0200

    feat: add new class for addition

---
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class AddApp {
+
+    static void main() {
+        System.out.println(add(1, 2));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
